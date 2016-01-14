2811fba55d921b0db1433759a299d5988fda0e63
JangCool <zerocooldog@naver.com>
2016-01-14 16:22:50 +0900

    * 의문 addShutdownHook은 어떤 상황에서 작동하며 제대로 실행이 되는가? 	 * 	 * Q1.main 메소드의 실행이 시작 된후 바로 종료 되는 경우. 	 *   - 프로그램 연산이 종료 됨과 동시에 바로 shutdownhook 실행 	 * 	 * Q2.main 메소드의 실행이 시작 된 후 프로그램 구동 상태에서 Ctrl+C 버튼을 눌러 강제 종료 하는 경우. 	 *   - 콘솔 중간에 종료할것인지 묻는 동시에 Addshutdownhook 작동. 	 * 	 * Q3.main 메소드의 실행이 시작 된 뒤 System.exit(0) 호출 하는 경우. 	 *   - 콘솔 중간에 종료할것인지 묻는 동시에 Addshutdownhook 작동. 	 * 	 * Q4.main 메소드의 실행이 시작 된 뒤 shell 의 kill 명령어로 강제 종료 하는 경우. 	 *   - kill 명령어로 죽이면 addShutdownHook이 먹지 않고 jvm이 강제로 종료됨

---
+++ src/ex/threadpool/AddShutdownHook.java
@@ -0,0 +1,67 @@
+package ex.threadpool;
+
+public class AddShutdownHook {
+
+	/**
+	 *  Q1.main 메소드의 실행이 시작 된후 바로 종료 되는 경우.
+	 *
+	 */
+	public void addShutdownHookQ1(){
+		Runtime.getRuntime().addShutdownHook(new Thread(){
+			@Override
+			public void run() {
+				System.out.println("shutdown hook...... execute Q1");
+			}
+		});
+	}
+	
+	/**
+	 *  Q2.main 메소드의 실행이 시작 된 후 프로그램 구동 상태에서 Ctrl+C 버튼을 눌러 강제 종료 하는 경우.
+	 *
+	 */
+	public void addShutdownHookQ2(){
+		Runtime.getRuntime().addShutdownHook(new Thread(){
+			@Override
+			public void run() {
+				System.out.println("shutdown hook...... execute Q2");
+			}
+		});
+		
+		
+		//shutdownHook을 등록 하고 프로그램 대기상태로 만들기 위해 sleep 호출.
+		try {
+			System.out.println("프로그램 구동 중......");
+			Thread.sleep(50000000);
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 *  Q3.main 메소드의 실행이 시작 된 뒤 System.exit(0) 호출 하는 경우.
+	 *
+	 */
+	public void addShutdownHookQ3(){
+		
+		Runtime.getRuntime().addShutdownHook(new Thread(){
+			@Override
+			public void run() {
+				System.out.println("shutdown hook...... execute Q3");
+			}
+		});
+		
+		
+		//shutdownHook을 등록 하고 프로그램 대기상태로 만들기 위해 sleep 호출.
+		try {
+			System.out.println("프로그램 구동 중......");
+			Thread.sleep(5000);
+			System.exit(0);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
